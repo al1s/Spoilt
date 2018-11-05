f0c1fddba30405b7ebd7520ca655c601c496c447
rebeccahong <rebeccayhong@gmail.com>
2018-11-05 14:10:53 -0800

    Add props to Spoiler model

--- Spoilt/Models/Spoiler.cs
+++ Spoilt/Models/Spoiler.cs
@@ -7,5 +7,13 @@ namespace Spoilt.Models
 {
     public class Spoiler
     {
+        // Model props:
+        public int ID { get; set; }
+        public string MovieID { get; set; }
+        public string Text { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        // Navigation props:
+        public Movie Movie { get; set; }
     }
 }
