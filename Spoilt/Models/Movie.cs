444bd2ca8033cf4b62bbe252f9d2c729e554bacf
rebeccahong <rebeccayhong@gmail.com>
2018-11-05 14:08:20 -0800

    Add props for Movie and MovieSpoilers

--- Spoilt/Models/Movie.cs
+++ Spoilt/Models/Movie.cs
@@ -7,5 +7,15 @@ namespace Spoilt.Models
 {
     public class Movie
     {
+        // Model Properties:
+        // Grab from IMDB string ID for movie
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Plot { get; set; }
+        public string Genre { get; set; }
+        public string PosterUrl { get; set; }
+
+        // Navigation properties
+        public ICollection<Spoiler> Spoilers { get; set; }
     }
 }
