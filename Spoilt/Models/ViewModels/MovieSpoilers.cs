492e82ea5a1685d239551e0dec6557d82e55d543
rebeccahong <rebeccayhong@gmail.com>
2018-11-05 14:12:47 -0800

    Update MovieSpoilers ViewModel

--- Spoilt/Models/ViewModels/MovieSpoilers.cs
+++ Spoilt/Models/ViewModels/MovieSpoilers.cs
@@ -8,11 +8,12 @@ namespace Spoilt.Models.ViewModels
     public class MovieSpoilers
     {
         // Model Properties:
-        // Grab from IMDB string ID for movie
-        public string ID { get; set; }
+        public string MovieID { get; set; }
         public string Title { get; set; }
         public string Plot { get; set; }
         public string Genre { get; set; }
         public string PosterUrl { get; set; }
+
+        public ICollection<Spoiler> Spoilers { get; set; }
     }
 }
