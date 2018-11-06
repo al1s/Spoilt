aad319aa2f8e127d685bfe0272b93f70ae4c398f
rebeccahong <rebeccayhong@gmail.com>
2018-11-06 15:29:09 -0800

    Add jquery and a script tag in Movies/Index

---
+++ Spoilt/Controllers/SessionsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spoilt.Controllers
+{
+    public class SessionsController : Controller
+    {
+        // GET: Sessions
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Sessions/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Sessions/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sessions/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Sessions/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Sessions/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Spoilt/Models/Interfaces/ISession.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spoilt.Models.Interfaces
+{
+    interface ISession
+    {
+        Task<Session> CreateSessionString();
+    }
+}
