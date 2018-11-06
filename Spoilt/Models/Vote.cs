1a260c535aa6939180a1468cde93d64071684913
rebeccahong <rebeccayhong@gmail.com>
2018-11-06 14:09:07 -0800

    Add Vote model props

--- Spoilt/Models/Vote.cs
+++ Spoilt/Models/Vote.cs
@@ -7,5 +7,13 @@ namespace Spoilt.Models
 {
     public class Vote
     {
+        // Model props
+        public int ID { get; set; }
+        public string MovieID { get; set; }
+        public int SpoilerID { get; set; }
+        public int Count { get => 1; }
+
+        // Navigation props
+        public Session Session { get; set; }
     }
 }
