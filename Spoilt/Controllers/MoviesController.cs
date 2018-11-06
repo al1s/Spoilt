e26f9ae5623dab44da968b1cc9240bb0c771b22c
Neth Horn <nethwebdev@gmail.com>
2018-11-06 14:13:42 -0800

    App is getting data from external spoiler API

--- Spoilt/Controllers/MoviesController.cs
+++ Spoilt/Controllers/MoviesController.cs
@@ -3,12 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Spoilt.Models;
+using Spoilt.Models.Interfaces;
+using Spoilt.Models.Services;
 
 namespace Spoilt.Controllers
 {
     public class MoviesController : Controller
     {
-        public IActionResult Index()
+        private readonly IMovie _movie;
+
+        public MoviesController(IMovie movieInterface)
+        {
+            _movie = movieInterface;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var myMovies = await _movie.GetMovies();
+            return View(myMovies);
+        }
+
+        public IActionResult Details(string id)
         {
             return View();
         }
---
+++ Spoilt/Models/Interfaces/IMovie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spoilt.Models.Interfaces
+{
+    public interface IMovie
+    {
+        // Get All
+        Task<IEnumerable<Movie>> GetMovies();
+
+        // Get Movies by Title
+        Task<IEnumerable<Movie>> GetMoviesByTitle(string title);
+
+        // Get One
+        Task<Movie> GetMovieById(string id);
+    }
+}
