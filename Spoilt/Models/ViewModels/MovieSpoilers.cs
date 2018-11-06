82721420337e0f0b10971c59976aaec4ca40e2e1
alstof <alstof@gmail.com>
2018-11-06 12:57:35 -0800

    Add details page with two partials

--- Spoilt/Models/ViewModels/MovieSpoilers.cs
+++ Spoilt/Models/ViewModels/MovieSpoilers.cs
@@ -8,11 +8,7 @@ namespace Spoilt.Models.ViewModels
     public class MovieSpoilers
     {
         // Model Properties:
-        public string MovieID { get; set; }
-        public string Title { get; set; }
-        public string Plot { get; set; }
-        public string Genre { get; set; }
-        public string PosterUrl { get; set; }
+        public Movie Movie { get; set; }
 
         // Collection of spoilers that belong to a particular movie
         public ICollection<Spoiler> Spoilers { get; set; }
