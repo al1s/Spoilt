31cdbfa9cb6a01cd65506d270eeabc026c589656
Neth Horn <nethwebdev@gmail.com>
2018-11-06 10:12:07 -0800

    Added views, controllers, and a layout

---
+++ Spoilt/Controllers/SpoilersController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Spoilt.Controllers
+{
+    public class SpoilersController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
