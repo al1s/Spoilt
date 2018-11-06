72384da7640c7347b709b5aae8c09d659dc7d8d8
rebeccahong <rebeccayhong@gmail.com>
2018-11-06 15:51:00 -0800

    Implement ISession in SessionService

--- Spoilt/Models/Services/SessionService.cs
+++ Spoilt/Models/Services/SessionService.cs
@@ -3,14 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Spoilt.Models.Interfaces;
+using Spoilt.Models;
+using Spoilt.Data;
 
 namespace Spoilt.Models.Services
 {
     public class SessionService : ISession
     {
-        Task<Session> ISession.CreateSessionString()
+        private SpoiltDbContext _context;
+
+        public SessionService(SpoiltDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+
+        public async Task<Session> CreateSessionString()
+        {
+            Session session = new Session();
+            _context.Sessions.Add(session);
+            await _context.SaveChangesAsync();
+
+            return session;
         }
     }
 }
