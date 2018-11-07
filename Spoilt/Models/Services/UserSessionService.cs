ff8017ec74848beca92b746ddb6448319b4acf79
alstof <alstof@gmail.com>
2018-11-07 06:42:21 -0800

    Fix naming

---
+++ Spoilt/Models/Services/UserSessionService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Spoilt.Models.Interfaces;
+using Spoilt.Models;
+using Spoilt.Data;
+
+namespace Spoilt.Models.Services
+{
+    public class UserSessionService : IUserSession
+    {
+        private SpoiltDbContext _context;
+
+        public UserSessionService(SpoiltDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<UserSession> CreateSessionString()
+        {
+            UserSession session = new UserSession();
+            _context.Sessions.Add(session);
+            await _context.SaveChangesAsync();
+
+            return session;
+        }
+    }
+}
---
+++ Spoilt/Models/UserSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spoilt.Models
+{
+    public class UserSession
+    {
+        // Model props:
+        // SessionID created via JS and stored in LocalStorage
+        public string ID { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        // Navigation prop
+        public ICollection<Vote> Votes { get; set; }
+    }
+}
