63a75ae57da5fbb9cd1190a15505d16df9ebcb1d
Neth Horn <nethwebdev@gmail.com>
2018-11-08 14:00:53 -0800

    Updated Readme

---
+++ Spoilt/Migrations/20181108215405_local.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Spoilt.Migrations
+{
+    public partial class local : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CreatedAt",
+                table: "Spoilers",
+                newName: "Created");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Created",
+                table: "Spoilers",
+                newName: "CreatedAt");
+        }
+    }
+}
