cd58c8cfb626051060b31c4238c97e5c259f17da
alstof <alstof@gmail.com>
2018-11-09 02:48:00 -0800

    Add About controller test

---
+++ SpoiltTests/AboutControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Spoilt.Controllers;
+using Xunit;
+
+namespace SpoiltTests
+{
+    public class AboutControllerTests
+    {
+        /// <summary>
+        /// Controller can return ViewResult with a valid Model
+        /// </summary>
+        /// <returns>Task</returns>
+        [Fact]
+        public void AboutIndex_ReturnsViewResult()
+        {
+            // arrange
+            var controller = new AboutController();
+
+            // act
+            var result = controller.Index();
+
+            // assert 
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
