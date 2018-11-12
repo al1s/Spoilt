dfbc346390e401fecc2711e2763d5f63821fd38b
alstof <alstof@gmail.com>
2018-11-11 17:33:29 -0800

    Add tests for Spoiler Edit action

--- SpoiltTests/SpoilersControllerTests.cs
+++ SpoiltTests/SpoilersControllerTests.cs
@@ -63,7 +63,7 @@ namespace SpoiltTests
         /// </summary>
         /// <returns>Task</returns>
         [Fact]
-        public async Task SpoilersCreatePost_WithIncorrectModel_ReturnsDBException()
+        public async Task SpoilersCreatePost_WithIncorrectModel_ReturnsTheSamePage()
         {
             // arrange
             string movieIdToSpoil = "ID1";
@@ -84,7 +84,7 @@ namespace SpoiltTests
             Assert.Equal(GetTestMovieWithSpoilers().Title, model.Title);
         }
         /// <summary>
-        /// Controller can return RedirectToActionResult with a valid redirection route
+        /// Controller can return NotFound for not matching IDs in method call parameter and object
         /// </summary>
         /// <returns>Task</returns>
         [Fact]
@@ -106,13 +106,85 @@ namespace SpoiltTests
             var viewResult = Assert.IsType<NotFoundResult>(result);
         }
 
+
+
+        /// <summary>
+        /// Controller can return ViewResult if invoked with a valid ID 
+        /// </summary>
+        /// <returns>Task</returns>
+        [Fact]
+        public async Task SpoilersEdit_ReturnsViewResultWithASpoilerModel()
+        {
+            // arrange
+            int SpoilerId= 1;
+            Spoiler spoiler = GetTestSpoiler();
+            var movieMockRepo = new Mock<IMovie>();
+            var spoilerMockRepo = new Mock<ISpoiler>();
+            spoilerMockRepo.Setup(repo => repo.GetSpoiler(SpoilerId)).ReturnsAsync(spoiler);
+            var controller = new SpoilersController(movieMockRepo.Object, spoilerMockRepo.Object);
+
+            // act
+            var result = await controller.Edit(SpoilerId);
+
+            // assert 
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<Spoiler>(viewResult.ViewData.Model);
+        }
+        /// <summary>
+        /// Controller can return RedirectToAction
+        /// </summary>
+        /// <returns>Task</returns>
+        [Fact]
+        public async Task SpoilersEdit_ReturnsRedirectToActionResult()
+        {
+            // arrange
+            int SpoilerId= 1;
+            string spoilerEditedText = "Edit1";
+            Spoiler spoiler = GetTestSpoiler();
+            var movieMockRepo = new Mock<IMovie>();
+            var spoilerMockRepo = new Mock<ISpoiler>();
+            spoilerMockRepo.Setup(repo => repo.GetSpoiler(SpoilerId)).ReturnsAsync(spoiler);
+            var controller = new SpoilersController(movieMockRepo.Object, spoilerMockRepo.Object);
+
+            // act
+            var result = await controller.Edit(SpoilerId, spoilerEditedText);
+
+            // assert 
+            var viewResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Equal("Details", viewResult.ActionName);
+            Assert.Equal("Movies", viewResult.ControllerName);
+        }
+        /// <summary>
+        /// Controller can return NotFound for incorrect spoiler ID
+        /// </summary>
+        /// <returns>Task</returns>
+        [Fact]
+        public async Task SpoilersEdit_ReturnsNotFoundForNoSuchSpoilerId()
+        {
+            // arrange
+            int SpoilerId= 1;
+            Spoiler spoiler = default(Spoiler);
+            string spoilerEditedText = "Edit1";
+            var movieMockRepo = new Mock<IMovie>();
+            var spoilerMockRepo = new Mock<ISpoiler>();
+            spoilerMockRepo.Setup(repo => repo.GetSpoiler(SpoilerId)).ReturnsAsync(spoiler);
+            var controller = new SpoilersController(movieMockRepo.Object, spoilerMockRepo.Object);
+
+            // act
+            var result = await controller.Edit(SpoilerId, spoilerEditedText);
+
+            // assert 
+            var viewResult = Assert.IsType<NotFoundResult>(result);
+        }
+
         private Spoiler GetTestSpoiler()
         {
             return new Spoiler()
             {
                 SpoilerText = "Spoiler1",
                 Votes = 1,
-                MovieID = "ID1"
+                MovieID = "ID1",
+                ID = 1
             };
         }
         private Movie GetTestMovieWithSpoilers()
